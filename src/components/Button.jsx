9450411161b193474bc7125ee45285e6575a0b4e
Arpit <156578394+arpitbhatia23@users.noreply.github.com>
2024-06-02 10:47:54 +0530

    add

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 function Button({children ,
     type='button',
@@ -8,8 +9,9 @@ function Button({children ,
     ...props
 
 }) {
+  const theme = useSelector(state=>state.theme.theme)
   return (
-   <button className={`px-4 py-4 rounded-lg ${bgColor} ${textcolor} ${className}`} {...props}>
+   <button className={`px-4 py-4 rounded-lg ${bgColor} ${textcolor} ${className} ${theme==="dark"? 'bg-blue-600':'bg-red-500 text-black'}`} {...props}>
     {children}
    </button>
   )
