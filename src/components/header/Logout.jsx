1df39b4d407d2f463dbe16f1cd4377f39259ecfd
Arpit <156578394+arpitbhatia23@users.noreply.github.com>
2024-06-02 07:25:03 +0530

    added

--- src/components/header/Logout.jsx
+++ src/components/header/Logout.jsx
@@ -10,7 +10,7 @@ function Logout() {
         toast.success("logout succesfully")
     }
   return (
-   <button className='inline-block px-6 py-2 duration-200   hover:bg-orange-500 rounded-full' onClick={logoutHandler}> logout </button>
+   <button className='inline-block px-6 py-2 duration-200    rounded-full' onClick={logoutHandler}> logout </button>
   )
 }
 
